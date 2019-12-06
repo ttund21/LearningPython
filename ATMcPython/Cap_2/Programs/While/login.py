ee5a3fc54fdb4cb21c97d273a39b779945a0a72c
Damocles <ttund21@gmail.com>
2019-12-06 15:42:31 -0300

    Add programa com o uso do while, atividade pratica.

--- ATMcPython/Cap_2/Programs/While/login.py
+++ ATMcPython/Cap_2/Programs/While/login.py
@@ -1,23 +1,25 @@
 luser = ''
 lpass = ''
-user= 'teste'
+user= 'admin'
+passwd = 'admin'
 
 print('WeLcOmE\n')
-print('If u want to create a new account, press 1. If not just hit enter')
+print('If u want to create a new account, press 1. If not just hit enter.')
 resp = input('>>> ')
 
 if resp == '1':
    print('\nWelcome to account creator. Now put your username and password.')
    user = input('Username: ')
    passwd = input('Password: ')
-else:
-    print('Sign In')
-    while True:       
-        luser = input('Username: ')
-        if luser != user:
-            continue
-        if luser == user:
-            break
-   
-    
+
+print('Sign In')
+while True:       
+    luser = input('\nUsername: ')
+    lpass = input('Password: ')
+    if luser == user and lpass == passwd:
+        print('\nACCESS GRANTED!!!!')
+        break
+    else:
+        print('\nYour login attempt failed. Verify that the username and password are correct.')
+        continue
 
