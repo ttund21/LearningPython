b4dab3a7e2acc75c0932a8364a4e54f8d48bc241
Damocles <ttund21@gmail.com>
2019-12-27 19:07:14 -0300

    Cap4 Finalizado, add exercicio de repeticao

--- ATMcPython/Cap4_Listas/ProjetoPratico/CodigoVirg.py
+++ ATMcPython/Cap4_Listas/ProjetoPratico/CodigoVirg.py
@@ -1,10 +1,10 @@
 def frase(lista):
     frase = ''
     for i in range(len(lista)):
-        if i < (len(lista) - 2):
-            frase += lista[i] + ', '
-        elif i == (len(lista) - 1):
+        if i == (len(lista) - 2):
             frase += lista[i]
+        elif i < (len(lista) - 2):
+            frase += lista[i] + ', '
         else:
             frase += ' and ' + lista[i]
     
